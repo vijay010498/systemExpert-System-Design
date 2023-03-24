afc630bb1040f56004d953f7af6867c4cf220a76
vijay010498 <svijayr007@gmail.com>
2023-03-24 09:59:18 +0530

    completed fundamentals intro

---
+++ contents.ts
@@ -0,0 +1,8 @@
+// Design Fundamentals
+// we have to explain why we designed in such as way
+// Basic fundamentals - client-server model, Network Protocols, Storage, etc
+
+
+// client-server model
+// client - talks to servers
+// server - talks to clients
