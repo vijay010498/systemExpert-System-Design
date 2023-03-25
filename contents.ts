44f95b72e265256c1e7148b75521776d145479f0
vijay010498 <svijayr007@gmail.com>
2023-03-25 12:01:00 +0530

    client-server model

--- contents.ts
+++ contents.ts
@@ -3,6 +3,13 @@
 // Basic fundamentals - client-server model, Network Protocols, Storage, etc
 
 
+
+// -- 3
+// what happens when u open a website ?
 // client-server model
 // client - talks to servers
 // server - talks to clients
+// client - request server
+// server -  response for the incoming network calls
+
+// First calls DNS -> ask for IP address
